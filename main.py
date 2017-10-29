af3e88c050a71acdd8f2ce2d2217bd850acb9782
Matteo <matteo.ita@gmail.com>
2017-10-29 14:39:10 +0000

    stacking classifier

--- main.py
+++ main.py
@@ -7,14 +7,24 @@ from patsy import dmatrices
 from pylab import rcParams
 
 from sklearn import metrics
+from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import VotingClassifier
+
+from mlxtend.classifier import StackingClassifier
 
 pd.set_option('display.max_rows', None, 'display.max_columns', None)
 
+estimator = []
+columns = ['date', 'time', 'temp', 'humid', 'light']
+
 #dataset = pd.read_csv("a.csv", header = None)
-dataset = pd.read_csv("mote_9.csv")
-dataset.columns = ['date', 'time', 'temp', 'humid', 'light']
+dataset_1 = pd.read_csv("mote_38.csv")
+dataset_1.columns = columns
+
+dataset_2 = pd.read_csv("mote_9.csv")
+dataset_2.columns = columns
 
 """
 plt.subplot(111)
@@ -39,33 +49,65 @@ plt.ylabel("frequency")
 
 #plt.show()
 
-dataset.drop(['date'], axis = 1, inplace = True)
-dataset.drop(['time'], axis = 1, inplace = True)
-dataset['hot'] = ((dataset.temp > 20) & (dataset.humid < 35) & (dataset.light > 400)).astype(int)
+dataset_1.drop(['date'], axis = 1, inplace = True)
+dataset_1.drop(['time'], axis = 1, inplace = True)
+dataset_1['hot'] = ((dataset_1.temp > 20) & (dataset_1.humid < 30) & (dataset_1.light > 400)).astype(int)
+
+dataset_2.drop(['date'], axis = 1, inplace = True)
+dataset_2.drop(['time'], axis = 1, inplace = True)
+dataset_2['hot'] = ((dataset_2.temp > 20) & (dataset_2.humid < 30) & (dataset_2.light > 400)).astype(int)
+
 #print dataset
 
 print "average group\n------------------\n"
-print dataset.groupby('hot').mean()
+print dataset_1.groupby('hot').mean()
+print "\n--------------------\n"
+print dataset_2.groupby('hot').mean()
 
-y, x = dmatrices('hot ~ temp + humid + light', dataset, return_type = 'dataframe')
-y = np.ravel(y) # 1D array
+y_1, x_1 = dmatrices('hot ~ temp + humid + light', dataset_1, return_type = 'dataframe')
+y_1 = np.ravel(y_1) # 1D array
+
+y_2, x_2 = dmatrices('hot ~ temp + humid + light', dataset_2, return_type = 'dataframe')
+y_2 = np.ravel(y_2)
 
 #x = dataset.ix[:, (0, 1, 2)].values
 #y = dataset.ix[:, 3].values
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.6, test_size = 0.4, random_state = 0)
+x_train_1, x_test_1, y_train_1, y_test_1 = train_test_split(x_1, y_1, train_size = 0.6, test_size = 0.4, random_state = 0)
+x_train_2, x_test_2, y_train_2, y_test_2 = train_test_split(x_2, y_2, train_size = 0.6, test_size = 0.4, random_state = 0)
 
-log_reg = LogisticRegression()
-log_reg.fit(x_train, y_train)
+logreg1 = LogisticRegression()
+logreg1.fit(x_train_1, y_train_1)
+logreg2 = LogisticRegression()
+logreg2.fit(x_train_2, y_train_2)
 
-predicted = log_reg.predict(x_test)
-#print predicted
-prob = log_reg.predict_proba(x_test)
-#print prob
+estimator.append(logreg1)
+estimator.append(logreg2)
 
-confmatrix = metrics.confusion_matrix(y_test, predicted)
-print "\n", confmatrix
+predicted_1 = logreg1.predict(x_test_1)
+prob_1 = logreg1.predict_proba(x_test_1)
+predicted_2 = logreg2.predict(x_test_2)
+prob_2 = logreg2.predict_proba(x_test_2)
 
-print "\naccuracy score:", metrics.accuracy_score(y_test, predicted)
-print log_reg.get_params(deep = True)
+confmatrix_1 = metrics.confusion_matrix(y_test_1, predicted_1)
+print "\nconfusion matrix\n-----------------\n", confmatrix_1
+
+confmatrix_2 = metrics.confusion_matrix(y_test_2, predicted_2)
+print "\nconfusion matrix\n-----------------\n", confmatrix_2
+
+print "\naccuracy score:", metrics.accuracy_score(y_test_1, predicted_1)
+print logreg1.get_params(deep = True)
+
+print "\naccuracy score:", metrics.accuracy_score(y_test_2, predicted_2)
+print logreg2.get_params(deep = True)
+
+#ensemble = VotingClassifier(estimator)
+#res = model_selection.cross_val_score(ensemble)
+#print res.mean()
+
+lr = LogisticRegression()
+sclf = StackingClassifier(classifiers = [logreg1, logreg2], meta_classifier = lr)
+
+#res = model_selection.cross_val_score(sclf, scoring = 'accuracy')
+#print res
 
