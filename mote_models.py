10ed15fbc47d9a6a7f788d2dd98fd8e832d7d6af
Matteo <matteo.ita@gmail.com>
2017-11-07 22:24:33 +0000

    threaded model fitting + iterate df for model coefs

--- mote_models.py
+++ mote_models.py
@@ -3,56 +3,100 @@ import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
 
-import statsmodels.api as sm
 from patsy import dmatrices
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-logging.basicConfig(level = logging.DEBUG, format = "(%threadName)-1s %(message)s")
+logging.basicConfig(level = logging.DEBUG, format = "(%(threadName)-1s) %(message)s")
 #pd.set_option('display.max_rows', None, 'display.max_columns', None)
+pd.options.mode.chained_assignment = None
 
-def logit_model(ids):
-	columns = ['date', 'time', 'temp', 'humid', 'lux']
-	mote_id = "mote_" + str(ids) + ".csv"
+class logitmodel(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
 	
-	dataset = pd.read_csv(mote_id)
-	dataset.columns = columns
+	def run(self):
+		datasets = [2, 9, 16, 27, 38, 47, 50]
+		for i in datasets:
+			t = threading.Thread(name = ("mote_" + str(i)), target = self.model, args = (i, ))
+			t.start()
+			#t.join()
 
-	"""
-	plots
-	---------------
-	"""
+	def model(self, ids):
+		s1 = 0
+		s2 = 10000
 
-	dataset.drop(['date'], axis = 1, inplace = True)
-	dataset.drop(['time'], axis = 1, inplace = True)
-	dataset['hot'] = ((dataset.temp > 20) & (dataset.humid < 30 ) & (dataset.lux > 400)).astype(int)
+		local_mote_coefs = {}
+		mote_ids = []
+		mote_ids.append(ids)
 
-	#print "average group\n----------------------\n"
-	#print dataset.groupby('hot').mean()
+
+		"""
+		plots
+		---------------
+		plt.subplot(111)
+		plt.subplots_adjust(bottom = 0.2, top = 0.9)
+		plt.plot_date(x = dataset.date, y = dataset.temp, fmt = 'r-')
+		plt.xticks(rotation = 90)
+
+		plt.scatter(dataset.temp, dataset.humid, s = 5, c = ['red', 'green'])
+		plt.xlabel("temp")
+		plt.ylabel("humid")
+
+		plt.subplot(2, 1, 1)
+		dataset.temp.hist()
+		plt.xlabel("temperature")
+		plt.ylabel("frequency")
+
+		plt.subplot(2, 1, 2)
+		dataset.light.hist()
+		plt.xlabel("lux")
+		plt.ylabel("frequency")
+		"""
+
+		#plt.show()
 	
-	y, x = dmatrices('hot ~ temp + humid + lux', dataset, return_type = 'dataframe')
-	y = np.ravel(y)
+		for _ in xrange(2):
+			columns = ['date', 'time', 'temp', 'humid', 'lux']
+			mote_id = "mote_" + str(ids) + ".csv"
+		
+			df = pd.read_csv(mote_id, header = 1)
+			dataset = df.iloc[s1:s2, :]
+			dataset.columns = columns
 
-	x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.6, test_size = 0.4, random_state = 0)
+			dataset.drop(['date'], axis = 1, inplace = True)
+			dataset.drop(['time'], axis = 1, inplace = True)
+			dataset['hot'] = ((dataset.temp > 20) & (dataset.humid < 30 ) & (dataset.lux > 400)).astype(int)
 
-	model = LogisticRegression()
-	model.fit(x_train, y_train)
+			#print "average group\n----------------------\n"
+			#print dataset.groupby('hot').mean()
+		
+			y, x = dmatrices('hot ~ temp + humid + lux', dataset, return_type = 'dataframe')
+			y = np.ravel(y)
 
-	predicted = model.predict(x_test)
-	prob = model.predict_proba(x_test)
+			x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.6, test_size = 0.4, random_state = 0)
 
-	#confmatrix = metrics.confusion_matrix(y_test, predicted)
-	#print "\nconfusion matrix\n-----------------------\n", confmatrix
-	#print "\naccuracy score:", metrics.accuracy_score(y_test, predicted)
+			model = LogisticRegression()
+			model.fit(x_train, y_train)
 
-	modelparams = model.get_params(deep = True)
-	modelcoefs = model.coef_
+			predicted = model.predict(x_test)
+			prob = model.predict_proba(x_test)
 
-	print modelcoefs
+			#confmatrix = metrics.confusion_matrix(y_test, predicted)
+			#print "\nconfusion matrix\n-----------------------\n", confmatrix
+			#print "\naccuracy score:", metrics.accuracy_score(y_test, predicted)
 
-if __name__ == "__main__":	
-	datasets = [2, 9, 38, 47, 50]
-	for ids in datasets:
-		t = threading.Thread(name = ("mote_", str(ids)), target = logit_model, args = (ids, ))
-		t.start()
+			modelparams = model.get_params(deep = True)
+			modelcoefs = model.coef_
+
+			local_mote_coefs.update({mote_id : modelcoefs})	
+
+			logging.debug("%d - %d\t\t%s", s1, s2, modelcoefs)
+			s1 = s2
+			s2 += 1200
+
+if __name__ == "__main__":
+	print "mote coefficient vectors [b0, b1, b2, b3]\n"	
+	m = logitmodel()
+	m.run()
